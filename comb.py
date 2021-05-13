3f5b3780c84fa0a5211a3265ebb4631f0c1154e4
Yuchen Wu <77533494+Chibadaisuki@users.noreply.github.com>
2021-05-13 12:57:57 +0800

    Add files via upload

---
+++ comb.py
@@ -0,0 +1,254 @@
+
+block_layer_list = [
+
+# Double layer test idx 39
+[
+    ["layers.0.weight"]
+],
+
+[
+    ['layers.1.weight',
+    'layers.1.bias']
+],
+[
+    ['layers.3.conv1.weight', 
+    'layers.3.conv2.weight'],
+    ['layers.3.conv1.weight', 
+    'layers.3.conv2.weight', 
+    'layers.3.bn1.weight',
+    'layers.3.bn1.bias',
+    'layers.3.bn2.weight',
+    'layers.3.bn2.bias'],
+],
+[
+    ['layers.4.conv1.weight',  
+    'layers.4.conv2.weight'],
+    ['layers.4.conv1.weight',  
+    'layers.4.conv2.weight',
+    'layers.4.bn1.weight',
+    'layers.4.bn1.bias',
+    'layers.4.bn2.weight',
+    'layers.4.bn2.bias']
+],
+[
+    ['layers.5.conv1.weight', 
+    'layers.5.conv2.weight'],
+    ['layers.5.conv1.weight', 
+    'layers.5.conv2.weight', 
+    'layers.5.bn1.weight',
+    'layers.5.bn1.bias',
+    'layers.5.bn2.weight',
+    'layers.5.bn2.bias']
+],
+[   
+    ['layers.6.conv1.weight', 
+    'layers.6.conv2.weight'
+    ],
+
+    ['layers.6.conv1.weight', 
+    'layers.6.conv2.weight',
+    'layers.6.shortcut.weight',
+    'layers.6.shortcut.bias'
+    ],
+    ['layers.6.conv1.weight', 
+    'layers.6.conv2.weight',
+    'layers.6.shortcut.weight',
+    ],
+    ['layers.6.conv1.weight', 
+    'layers.6.conv2.weight',
+    'layers.6.bn1.weight',
+    'layers.6.bn1.bias',
+    'layers.6.bn2.weight',
+    'layers.6.bn2.bias',
+    'layers.6.shortcut.weight',
+    'layers.6.shortcut.bias'],
+    ['layers.6.conv1.weight', 
+    'layers.6.conv2.weight',
+    'layers.6.bn1.weight',
+    'layers.6.bn1.bias',
+    'layers.6.bn2.weight',
+    'layers.6.bn2.bias']
+],
+
+[
+    ['layers.7.conv1.weight', 
+    'layers.7.conv2.weight'],
+
+    ['layers.7.conv1.weight', 
+    'layers.7.conv2.weight',
+    'layers.7.bn1.weight',
+    'layers.7.bn1.bias',
+    'layers.7.bn2.weight',
+    'layers.7.bn2.bias']
+],
+
+[
+    ['layers.8.conv1.weight', 
+    'layers.8.conv2.weight'],
+    ['layers.8.conv1.weight', 
+    'layers.8.conv2.weight',
+    'layers.8.bn1.weight',
+    'layers.8.bn1.bias',
+    'layers.8.bn2.weight',
+    'layers.8.bn2.bias']
+],
+
+[
+    ['layers.9.conv1.weight', 
+    'layers.9.conv2.weight'],
+    ['layers.9.conv1.weight', 
+    'layers.9.conv2.weight',
+    'layers.9.bn1.weight',
+    'layers.9.bn1.bias',
+    'layers.9.bn2.weight',
+    'layers.9.bn2.bias']
+],
+
+[
+    ['layers.10.conv1.weight', 
+    'layers.10.conv2.weight'
+    ],
+    ['layers.10.conv1.weight', 
+    'layers.10.conv2.weight',
+    'layers.10.shortcut.weight',
+    'layers.10.shortcut.bias'],
+    ['layers.10.conv1.weight', 
+    'layers.10.conv2.weight',
+    'layers.10.shortcut.weight'],
+    ['layers.10.conv1.weight', 
+    'layers.10.conv2.weight',
+    'layers.10.bn1.weight',
+    'layers.10.bn1.bias',
+    'layers.10.bn2.weight',
+    'layers.10.bn2.bias'],
+    ['layers.10.conv1.weight', 
+    'layers.10.conv2.weight',
+    'layers.10.bn1.weight',
+    'layers.10.bn1.bias',
+    'layers.10.bn2.weight',
+    'layers.10.bn2.bias',
+    'layers.10.shortcut.weight',
+    'layers.10.shortcut.bias']
+],
+
+[    
+    ['layers.11.conv1.weight', 
+    'layers.11.conv2.weight'],
+
+    ['layers.11.conv1.weight', 
+    'layers.11.conv2.weight',
+    'layers.11.bn1.weight',
+    'layers.11.bn1.bias',
+    'layers.11.bn2.weight',
+    'layers.11.bn2.bias']
+],
+[
+    ['layers.12.conv1.weight', 
+    'layers.12.conv2.weight'],
+    ['layers.12.conv1.weight', 
+    'layers.12.conv2.weight',
+    'layers.12.bn1.weight',
+    'layers.12.bn1.bias',
+    'layers.12.bn2.weight',
+    'layers.12.bn2.bias']
+],
+[    
+    ['layers.13.conv1.weight', 
+    'layers.13.conv2.weight'],
+    ['layers.13.conv1.weight', 
+    'layers.13.conv2.weight',
+    'layers.13.bn1.weight',
+    'layers.13.bn1.bias',
+    'layers.13.bn2.weight',
+    'layers.13.bn2.bias']
+],
+[    
+    ['layers.14.conv1.weight', 
+    'layers.14.conv2.weight'],
+
+    ['layers.14.conv1.weight', 
+    'layers.14.conv2.weight',
+    'layers.14.bn1.weight',
+    'layers.14.bn1.bias',
+    'layers.14.bn2.weight',
+    'layers.14.bn2.bias']
+],
+[    
+    ['layers.15.conv1.weight', 
+    'layers.15.conv2.weight'],
+    ['layers.15.conv1.weight', 
+    'layers.15.conv2.weight',
+    'layers.15.bn1.weight',
+    'layers.15.bn1.bias',
+    'layers.15.bn2.weight',
+    'layers.15.bn2.bias']
+],
+[    
+    ['layers.16.conv1.weight', 
+    'layers.16.conv2.weight'],
+    ['layers.16.conv1.weight', 
+    'layers.16.conv2.weight',
+    'layers.16.shortcut.weight',
+    'layers.16.shortcut.bias'],
+    ['layers.16.conv1.weight', 
+    'layers.16.conv2.weight',
+    'layers.16.shortcut.weight'],
+    ['layers.16.conv1.weight', 
+    'layers.16.conv2.weight',
+    'layers.16.bn1.weight',
+    'layers.16.bn1.bias',
+    'layers.16.bn2.weight',
+    'layers.16.bn2.bias'],
+    ['layers.16.conv1.weight', 
+    'layers.16.conv2.weight',
+    'layers.16.bn1.weight',
+    'layers.16.bn1.bias',
+    'layers.16.bn2.weight',
+    'layers.16.bn2.bias',
+    'layers.16.shortcut.weight',
+    'layers.16.shortcut.bias'],
+],
+[    
+    ['layers.17.conv1.weight', 
+    'layers.17.conv2.weight'],
+    ['layers.17.conv1.weight', 
+    'layers.17.conv2.weight',
+    'layers.17.bn1.weight',
+    'layers.17.bn1.bias',
+    'layers.17.bn2.weight',
+    'layers.17.bn2.bias'],
+],
+[    
+    ['layers.18.conv1.weight', 
+    'layers.18.conv2.weight'],
+    ['layers.18.conv1.weight', 
+    'layers.18.conv2.weight',
+    'layers.18.bn1.weight',
+    'layers.18.bn1.bias',
+    'layers.18.bn2.weight',
+    'layers.18.bn2.bias']
+]
+
+]
+
+from itertools import combinations
+
+# Indexes
+comb = combinations(range(len(block_layer_list)), 2)
+for idx_a, idx_b in comb:
+
+    la = block_layer_list[idx_a]
+    lb = block_layer_list[idx_b]
+
+    # Iterate through idex_a;
+    for weight_list_a in la:
+        # Iterate through idex_b
+        for weight_list_b in lb:
+            # Weight list to filename
+            fn_a = xxxxx(weight_list_a)
+            fn_b = xxxxx(weight_list_b)
+
+            # Evaluate
+            acc = yyyyy(fn_a, fn_b)
+
+            # Save result
